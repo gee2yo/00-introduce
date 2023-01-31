e43cfb96c38347bb9e25bd04a8bf11b6f720edf9
gee2yo <helloimgee2yo@gmail.com>
2023-01-31 22:37:37 +0900

    230131 모듈 추가 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">hello</div>;
+    </>
+  );
 }
 
 export default App;
